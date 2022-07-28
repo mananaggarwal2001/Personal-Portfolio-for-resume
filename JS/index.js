8a488cbc16a72015c871223c1c4ccf20e58e7972
mananaggarwal2001 <mananaggarwal2001@gmail.com>
2022-07-28 19:15:28 +0530

    intial commit

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+let typed = new Typed('.wordchangingclass',{
+    strings:["Manan Aggarwal.","a Content Creater.","a Coder.","a Writer.","a Guider.","a Freelancer."],
+    typeSpeed: 20,
+    backSpeed: 20,
+    loop:true,
+    loopCount:Infinity
+})
