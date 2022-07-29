c3f047425030fd1ef117ee5a3bf2faa61ca2d030
mananaggarwal2001 <mananaggarwal2001@gmail.com>
2022-07-29 17:04:57 +0530

    chnagesmade 3

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+AOS.init();
 let typed = new Typed('.wordchangingclass',{
     strings:["Manan Aggarwal.","a Content Creater.","a Coder.","a Writer.","a Guider.","a Freelancer."],
     typeSpeed: 20,
