99b439e195ba000506f8938e973876ac911a05a8
mananaggarwal2001 <mananaggarwal2001@gmail.com>
2022-07-31 20:41:42 +0530

    added images and the pictures

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,25 @@
 AOS.init();
-let typed = new Typed('.wordchangingclass',{
-    strings:["Manan Aggarwal.","a Content Creater.","a Coder.","a Writer.","a Guider.","a Freelancer."],
+let typed = new Typed('.wordchangingclass', {
+    strings: ["Manan Aggarwal.", "a Content Creater.", "a Coder.", "a Writer.", "a Guider.", "a Freelancer."],
     typeSpeed: 20,
     backSpeed: 20,
-    loop:true,
-    loopCount:Infinity
-})
+    loop: true,
+    loopCount: Infinity
+});
+let coverarrowdiv = document.getElementsByClassName(".coverarrowclass");
+let imageclass = document.getElementsByClassName(".coverarrowimageclass");
+function slideUp() {
+    imageclass.style.transition = "all 2s ease-in-out";
+    imageclass.style.height = "0px";
+}
+
+function slideDown() {
+    imageclass.style.transition = "all 2s ease-in-out";
+    imageclass.style.height = "100px";
+}
+
+setInterval(() => {
+
+    slideUp;
+    slideDown;
+}, 2000);
